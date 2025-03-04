cb5715c2ec9c698e216c0aa99d52ea7e9d86594e
Maria Jose Saad Plata <maria.saad.35@outlook.com>
2025-03-04 18:55:08 -0500

    se crea main con opciones

---
+++ main.py
@@ -0,0 +1,12 @@
+nameVendor=None
+products=[]
+product={}
+
+option=None
+
+print("Mercadito chévere")
+print("1. Crear lista de mercado")
+print("5. Salir")
+
+while opcion!=5:
+    opcion=int(input("Digita una opción"))
