4adf74d0de26f3b37b722e9c81c39068e4f95854
Maria Jose Saad Plata <maria.saad.35@outlook.com>
2025-03-04 20:56:57 -0500

    feat se crea opcion 1 para poblar diccionario y lista de productos

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 nameVendor=None
 products=[]
 product={}
@@ -6,7 +7,29 @@ option=None
 
 print("Mercadito chévere")
 print("1. Crear lista de mercado")
+print("2. Ver lista de mercado")
+print("3. Editar producto de la lista")
+print("4. Retirar producto de la lista")
 print("5. Salir")
 
-while opcion!=5:
-    opcion=int(input("Digita una opción"))
+while option!=5:
+    option=int(input("Digita una opción: "))
+    if option == 1:
+        print("Has ingresado a CREAR TU LISTA DE MERCADO")
+        product["id"]=random.randint(1000,9999)
+        product["name"]=input("Digita el nombre del producto: ")
+        product["price"]=int(input("Digita el precio del producto: "))
+        product["quantity"]=int(input("Digita la cantidad del producto: "))
+
+        products.append(product)
+        print(products)
+
+
+    elif option == 2:
+        print()
+    elif option == 3:
+        print()
+    elif option == 4:
+        print()
+    else:
+        print("Opción no válida")
